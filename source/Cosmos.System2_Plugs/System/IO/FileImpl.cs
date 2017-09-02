296fd7e45e08d5009b1d0ca50cd1672835053e7e
Alexy <alexy.da-cruz@live.fr>
2017-09-02 23:24:09 +0200

    Add Move Impl

--- source/Cosmos.System2_Plugs/System/IO/FileImpl.cs
+++ source/Cosmos.System2_Plugs/System/IO/FileImpl.cs
@@ -155,6 +155,26 @@ namespace Cosmos.System_Plugs.System.IO
             }
         }
 
+        public static void Move(string srcFile, string destFile)
+        {
+            if (File.Exists(destFile))
+            {
+                throw new IOException();
+            }
+            else
+            {
+                try
+                {
+                    byte[] srcFileBytes = File.ReadAllBytes(srcFile);
+                    File.WriteAllBytes(destFile, srcFileBytes);
+                }
+                catch (IOException ioEx)
+                {
+                    throw new IOException("File Move", ioEx);
+                }
+            }            
+        }
+
         public static void Copy(string srcFile, string destFile)
         {
             try
